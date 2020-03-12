db2f3253da4fa73d0282d513b3b4c03eafb66f40
Nick Stevens <nicholasstevens@hotmail.co.uk>
2020-03-12 12:30:38 +0000

    Changed xhr to fetch

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,96 +1,74 @@
-const weatherCountries = {
-   'maldives': {
+const weatherCountries = [
+   {
+      name: 'maldives',
       lat: 3,
       long: 73
    },
-   'rhodes': {
+   {
+      name: 'rhodes',
       lat: 36,
-      long: 28,
+      long: 28
    },
-   'lisbon': {
+   {
+      name: 'lisbon',
       lat: 39,
       long: 9
    },
-   'marrakesh': {
+   {
+      name: 'marrakech',
       lat: 31,
-      long: 8,
-   }
-}
-
+      long: 8
+   },
+]
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 const appId = "30d97b87215b9e9e7b0c5a065f302d8d";
 
 const getData = () => {
-   const xhr = new XMLHttpRequest();
    for (const country in weatherCountries) {
       console.log(country);
-      const url = `${baseUrl}?lat=${weatherCountries[country].lat}&lon=${weatherCountries[country].long}&APPID=${appId}`
-      xhr.open("GET", url);
-      xhr.send();
-
-      xhr.onreadystatechange = function () {
-         if (this.readyState == 4 && this.status == 200) {
-            const response = JSON.parse(this.responseText);
-            console.log(response);
-            conditions = response.weather[0].main;
-            temperature = response.main.temp - 273.15;
+      fetch(`${baseUrl}?lat=${weatherCountries[country].lat}&lon=${weatherCountries[country].long}&APPID=${appId}`)
+         .then(res => res.json())
+         .then(data => {
+            conditions = data.weather[0].main;
+            temperature = data.main.temp - 273.15;
             temperature = Math.round(temperature);
-            document.getElementById(country).innerHTML = `<h5>Current Weather</h5>
+            document.getElementById(weatherCountries[country].name).innerHTML = `<h5>Current Weather</h5>
                <table>
                   <tr><th>Conditions:</th><th>Temperature:</th></tr>
                   <tr><td>${conditions}</td><td>${temperature}</td></tr>
                </table>`;
-         }
-      };
+         })
    }
 }
 
 getData()
 
-// function getData(cb) {
-//    var xhr = new XMLHttpRequest();
+// const getData = () => {
+//    const xhr = new XMLHttpRequest();
+//    for (const country in weatherCountries) {
 
-//    xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=3&lon=73&APPID=30d97b87215b9e9e7b0c5a065f302d8d");
-//    xhr.send();
+//       console.log(country);
+//       const url = `${baseUrl}?lat=${weatherCountries[country].lat}&lon=${weatherCountries[country].long}&APPID=${appId}`;
+//       console.log(url);
+//       xhr.open("GET", url);
+//       xhr.send();
 
-//    xhr.onreadystatechange = function () {
-//       if (this.readyState == 4 && this.status == 200) {
-//          cb(JSON.parse(this.responseText));
-//       }
-//    };
-// };
+//       xhr.onreadystatechange = function () {
+//          if (this.readyState == 4 && this.status == 200) {
+//             const response = JSON.parse(this.responseText);
+//             console.log(response);
+//             conditions = response.weather[0].main;
+//             temperature = response.main.temp - 273.15;
+//             temperature = Math.round(temperature);
+//             document.getElementById(country).innerHTML = `<h5>Current Weather</h5>
+//                <table>
+//                   <tr><th>Conditions:</th><th>Temperature:</th></tr>
+//                   <tr><td>${conditions}</td><td>${temperature}</td></tr>
+//                </table>`;
+//          }
+//       };
+//    }
+// }
 
-// var temperature
-// var conditions
 
-// getData(function (data) {
-//    conditions = data.weather[0].main;
-//    temperature = data.main.temp - 273.15;
-//    temperature = Math.round(temperature);
-//    document.getElementById("data").innerHTML = `<h5>Current Weather</h5><table><tr><th>Conditions:</th><th>Temperature:</th></tr>
-//    <tr><td>${conditions}</td><td>${temperature}</td></tr></table>`
-// });
-
-// function getData2(cb) {
-//    var xhr = new XMLHttpRequest();
-
-//    xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?lat=36&lon=28&APPID=30d97b87215b9e9e7b0c5a065f302d8d");
-//    xhr.send();
-
-//    xhr.onreadystatechange = function () {
-//       if (this.readyState == 4 && this.status == 200) {
-//          cb(JSON.parse(this.responseText));
-//       }
-//    };
-// };
-// var temperatureRhodes
-// var conditionsRhodes
-
-// getData2(function (dataRhodes) {
-//    conditionsRhodes = dataRhodes.weather[0].main;
-//    temperatureRhodes = dataRhodes.main.temp - 273.15;
-//    temperatureRhodes = Math.round(temperatureRhodes);
-//    document.getElementById("data-rhodes").innerHTML = `<h5>Current Weather</h5><table><tr><th>Conditions:</th><th>Temperature:</th></tr>
-//    <tr><td>${conditionsRhodes}</td><td>${temperatureRhodes}</td></tr></table>`
-// });
-
+// getData()
