8e467922cea79d3e47763623d9648f8de70e45ee
Nick Stevens <nicholasstevens@hotmail.co.uk>
2020-03-12 09:38:03 +0000

    Added onclick to change search

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -3,6 +3,18 @@ const weatherCountries = {
       lat: 3,
       long: 73
    },
+   'rhodes': {
+      lat: 36,
+      long: 28,
+   },
+   'lisbon': {
+      lat: 39,
+      long: 9
+   },
+   'marrakesh': {
+      lat: 31,
+      long: 8,
+   }
 }
 
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
@@ -11,6 +23,7 @@ const appId = "30d97b87215b9e9e7b0c5a065f302d8d";
 const getData = () => {
    const xhr = new XMLHttpRequest();
    for (const country in weatherCountries) {
+      console.log(country);
       const url = `${baseUrl}?lat=${weatherCountries[country].lat}&lon=${weatherCountries[country].long}&APPID=${appId}`
       xhr.open("GET", url);
       xhr.send();
@@ -18,6 +31,7 @@ const getData = () => {
       xhr.onreadystatechange = function () {
          if (this.readyState == 4 && this.status == 200) {
             const response = JSON.parse(this.responseText);
+            console.log(response);
             conditions = response.weather[0].main;
             temperature = response.main.temp - 273.15;
             temperature = Math.round(temperature);
